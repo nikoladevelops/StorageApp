4f26089d9d343751ae4fb08b5bd67dda1221cabd
Nikola P. <nikoladevelops@gmail.com>
2024-05-17 19:55:14 +0300

    Add project files.

---
+++ StorageApp/Models/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StorageApp.Models
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options):base(options)
+        {
+        }
+
+        public DbSet<Item> Items { get; set; }
+    }
+}
---
+++ StorageApp/Models/Item.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StorageApp.Models
+{
+    public class Item
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "The Name field is required.")]
+        [MaxLength(30, ErrorMessage = "The Name cannot be longer than 30 characters.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "The Quantity field is required.")]
+        [Range(0, int.MaxValue, ErrorMessage = "The Quantity must be a non-negative integer.")]
+        public int Quantity { get; set; }
+
+        [Required(ErrorMessage = "The Price field is required.")]
+        [Range(0, double.MaxValue, ErrorMessage = "The Price must be a non-negative value.")]
+        public double Price { get; set; }
+
+        [Required(ErrorMessage = "The Supplier field is required.")]
+        [MaxLength(30, ErrorMessage = "The Supplier cannot be longer than 30 characters.")]
+        public string Supplier { get; set; }
+    }
+}
