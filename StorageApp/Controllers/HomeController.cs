571a64a69a4afcc8048052f01bd082d3c1edccf0
Nikola P. <nikoladevelops@gmail.com>
2024-05-18 11:44:30 +0300

    Worked a little bit on the design

--- StorageApp/Controllers/HomeController.cs
+++ StorageApp/Controllers/HomeController.cs
@@ -7,21 +7,43 @@ namespace StorageApp.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
+        // Additional parameters for filtering
+        [HttpGet]
         public IActionResult Index()
         {
+            var allItems = _context.Items.ToList();
+            return View(allItems);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            // TODO creating a new Item
             return View();
         }
 
-        public IActionResult Privacy()
+        [HttpGet]
+        public IActionResult Update()
         {
+            // TODO update an item by receiving a dto containing the updated info
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Delete()
+        {
+            // TODO deleting 
+            return View();
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
